20d84d5a9524cfea238f45efe18b5ac7ebec0def
Shahzaib Ali Hassan <shahzaibalihassan@gmail.com>
2021-10-08 20:11:04 +0500

    Added packages screen

---
+++ packages.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta property="og:title" content="">
+    <meta property="og:type" content="">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
+
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="apple-touch-icon" href="icon.png">
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/components.css">
+    <link rel="stylesheet" href="css/app.css">
+
+    <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+    <div class="container mt-5">
+        <div class="row top-header">
+            <div class="main-heading">
+                <h2>Clive Bible <br>Logo</h2>
+            </div>
+            <div class="avatar-img"><img src="img/img_avatar.png" alt="Avatar" class="avatar"></div>
+            <div class="user-name">
+                <h4>Welcome User</h4>
+            </div>
+        </div>
+
+        <div class="panel panel-default">
+            <div class="panel-body p-4">
+                <div class="freeplan">
+                    <h4>Free Plan</h4>
+                    <p>Limited Features</p>
+                    <div class="activate-btn"><button class="btn btn-danger">Activate</button></div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+    <script src="js/vendor/modernizr-3.11.2.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.1/js/bootstrap.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+
+    <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+    <script>
+        window.ga = function() {
+            ga.q.push(arguments)
+        };
+        ga.q = [];
+        ga.l = +new Date;
+        ga('create', 'UA-XXXXX-Y', 'auto');
+        ga('set', 'anonymizeIp', true);
+        ga('set', 'transport', 'beacon');
+        ga('send', 'pageview')
+    </script>
+    <script src="https://www.google-analytics.com/analytics.js" async></script>
+</body>
+
+</html>
