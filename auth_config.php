bb2e519d061ed8ce0b4fd94c93453578221ed252
Shahzaib Ali Hassan <shahzaibalihassan@gmail.com>
2021-10-09 21:25:17 +0500

    Remove secrret keys from config file

--- auth_config.php
+++ auth_config.php
@@ -4,9 +4,9 @@ require_once 'vendor/autoload.php';
 
 $google_client = new Google_Client();
 
-$google_client->setClientId('86385886443-j5183rr9dc5nh9inmvi0iasdpc84ccd2.apps.googleusercontent.com');
+$google_client->setClientId('');
 
-$google_client->setClientSecret('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');
+$google_client->setClientSecret('');
 
 $google_client->setRedirectUri('http://localhost/Bible-Verse-Reader/auth.php');
 
